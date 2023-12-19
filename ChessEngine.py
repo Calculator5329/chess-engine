636ecfcc468e3f95e1b276e719776fee4f6ef07d
Ethan <et2boys@hotmail.com>
2023-12-19 12:21:13 -0600

    Initial Commit. Displays static chessboard.

---
+++ ChessEngine.py
@@ -0,0 +1,21 @@
+# This class stores information about the current state of the game
+# It will also determine the valid moves at the current state and keep a move log
+
+class GameState():
+    def __init__(self):
+        # Board will be a 2d list, each element has 2 chars.
+        # First is color of piece, second is type of piece (ex: "bB" is black bishop)
+        # "--" Represents empty space
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+        pass
